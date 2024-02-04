3b7567bcbec3e037adc5f025d0e44b06ac966916
serious_dev <borisdrovnin@gmail.com>
2024-02-05 00:05:04 +0300

    step 2 // add -l

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::File};
+use std::{env, fs::File, io::{BufRead, BufReader}, process::exit};
 
 fn main() {
     let mut path = String::from("");
@@ -12,16 +12,31 @@ fn main() {
         }
     }
 
+    if command == "" || path == "" {
+        println!("Error: no argumants");
+        println!("Usage: `wc -c file.txt` or `wc -l file.txt` ");
+        exit(1);
+    }
+
+    let file = File::open(path.clone()).unwrap();
+
     match command.as_str() {
         "-c" => {
-            let file = File::open(path.clone()).unwrap();
             let len = file.metadata().unwrap().len();
             println!("{} {}", len, path);
+        }
+        "-l" => {
+            let mut lines = 0;
+            let mut buf = String::new();
+            let mut reader = BufReader::new(file);
+            while reader.read_line(&mut buf).unwrap() != 0 {
+                lines += 1;
+                buf.clear();
+            }
+            println!("{} {}", lines, path);
         }
         _ => {
             println!("Unsupported arguments: {} {}", command, path);
         }
     }
 }
-
-
