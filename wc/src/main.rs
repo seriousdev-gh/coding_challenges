37b385bd20e876c65d52f7459dccf51e7ce17dbf
serious_dev <borisdrovnin@gmail.com>
2024-02-04 23:22:54 +0300

    step 1 // add -c

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,3 +1,27 @@
+use std::{env, fs::File};
+
 fn main() {
-    println!("Hello, world!");
+    let mut path = String::from("");
+    let mut command = String::from(""); 
+
+    for arg in env::args().skip(1) {
+        if arg.starts_with("-") {
+            command = arg;
+        } else {
+            path = arg;
+        }
+    }
+
+    match command.as_str() {
+        "-c" => {
+            let file = File::open(path.clone()).unwrap();
+            let len = file.metadata().unwrap().len();
+            println!("{} {}", len, path);
+        }
+        _ => {
+            println!("Unsupported arguments: {} {}", command, path);
+        }
+    }
 }
+
+
